e4c01636cb1d4deac57ba9278caafd7d3bcd4e21
naggopigithuba <111102209+naggopigithuba@users.noreply.github.com>
2022-10-18 12:31:40 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\gopi123-bot-8a6f-Web-Deploy.pubxml -p:Password=5ciTpBjLmgRZeYbhxeQQiKiwAJjgtNsjjdpgfskJxxhdmGlw68TLjp4soSd6
+
